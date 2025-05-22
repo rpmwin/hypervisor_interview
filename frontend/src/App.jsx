f61a99c1dad9a6289df66e4ef4f8844fde122990
Rishik puneet m <rishikpuneetm@gmail.com>
2025-05-22 16:40:36 +0530

    home changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,16 @@ function App() {
     return (
         <>
             <Outlet />
+            use localhost:5173/all for displaying all the data
+            <br />
+             use
+            localhost:5173/add for add the transaction 
+            <br />
+            use localhost:5173/edit
+            for edit the transaction 
+            <br />
+            use localhost:5173/delete for delete the
+            transaction
         </>
     );
 }
